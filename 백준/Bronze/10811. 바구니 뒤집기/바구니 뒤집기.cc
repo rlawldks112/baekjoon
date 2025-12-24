f53270ff937293f021f9c1d89848ee0b8986073a
Jiahn Kim <jiminji1102@gmail.com>
2025-12-24 16:16:02 +0900

    [Bronze II] Title: 바구니 뒤집기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/10811. 바구니 뒤집기/바구니 뒤집기.cc
+++ 백준/Bronze/10811. 바구니 뒤집기/바구니 뒤집기.cc
@@ -1,9 +1,10 @@
 #include <iostream>
+#include <algorithm>
 using namespace std;
 int main(){
     int N,M;
     cin>> N>>M;
-    int A[N];
+    int A[N+1];
     for(int i =0; i<N; i++){
         A[i]=i+1;
     }
@@ -12,12 +13,7 @@ int main(){
         int i,j;
         cin >>i>>j;
         i-=1;j-=1;
-        while(i != j && i<j){
-            int temp = A[i];
-            A[i]= A[j];
-            A[j] = temp;
-            i++;j--;
-        }
+        reverse(A+i,A+j+1);
     }
     for(int i=0; i< N;i++){
         cout<<A[i]<<' ';
